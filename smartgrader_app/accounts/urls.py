fae85e64ef8ef9dd5ffdae530570f8cc34c00a29
padure11 <alexandru.padure04@e-uvt.ro>
2025-12-04 22:14:24 +0200

    Started functions for register/ login users

--- smartgrader_app/accounts/urls.py
+++ smartgrader_app/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path 
 from . import views
+from .views import register_user, login_user
 
 urlpatterns = [
-    path('', views.landing, name = 'landing')
+    path('', views.landing, name = 'landing'),
+    path("register/", register_user, name="register"),
+    path("login/", login_user, name="login"),
 ]
