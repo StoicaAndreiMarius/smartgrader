f90d8e19bb299c1a67a496f4ad9ec5df1d6a9940
padure11 <alexandru.padure04@e-uvt.ro>
2025-12-04 22:14:24 +0200

    Started functions for register/ login users

--- smartgrader_app/accounts/views.py
+++ smartgrader_app/accounts/views.py
@@ -1,6 +1,51 @@
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+import json
 
-# Create your views here.
+User = get_user_model()
+
 def landing(request):
     return render(request, 'accounts/landing.html')
+
+@csrf_exempt
+def register_user(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Only POST allowed"}, status=400)
+
+    data = json.loads(request.body)
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return JsonResponse({"error": "Email and password required"}, status=400)
+
+    if User.objects.filter(email=email).exists():
+        return JsonResponse({"error": "Email already registered"}, status=400)
+
+    user = User.objects.create_user(email=email, password=password)
+
+    return JsonResponse({"message": "User created successfully", "email": user.email}, status=201)
+
+
+@csrf_exempt
+def login_user(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Only POST allowed"}, status=400)
+
+    data = json.loads(request.body)
+
+    email = data.get("email")
+    password = data.get("password")
+
+    user = authenticate(request, email=email, password=password)
+
+    if user is None:
+        return JsonResponse({"error": "Invalid credentials"}, status=400)
+
+    login(request, user)
+
+    return JsonResponse({"message": "Login successful", "email": user.email})
