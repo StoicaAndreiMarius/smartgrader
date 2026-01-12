05037b9cb445186d6cdfd9215927ab1fff8f1c5c
sSlepa <adriangaspar71@gmail.com>
2026-01-12 21:19:07 +0200

    Add student submission deletion and info page routes
    - Add deletion route for students to remove their submissions
    - Add about and support page routes for footer links
    - Maintain existing routes for help, privacy, and terms pages

--- smartgrader_app/test_grader/urls.py
+++ smartgrader_app/test_grader/urls.py
@@ -19,8 +19,13 @@ urlpatterns = [
     path('student/test/<str:share_code>/', views.student_test_access, name='student-test-access'),
     path('student/test/<str:share_code>/submit/', views.student_submit_answers, name='student-submit-answers'),
     path('student/test/<str:share_code>/result/<int:submission_id>/', views.student_submission_result, name='student-submission-result'),
+    path('student/test/<str:share_code>/result/<int:submission_id>/delete/', views.student_delete_submission, name='student-delete-submission'),
 
     # General pages
     path('help/', views.help_page, name='help'),
     path('information/', views.information_page, name='information'),
+    path('about/', views.about_page, name='about'),
+    path('support/', views.support_page, name='support'),
+    path('privacy/', views.privacy_page, name='privacy'),
+    path('terms/', views.terms_page, name='terms'),
 ]
